a0900526bbdfd409cd90aabd96404a2418c2452e
Victor Vulovic <victor@humanify.co>
2015-04-02 10:25:41 -0700

    Updated home page, added buttons, adjusted css, and added navbar

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root 'pages#home'
-  get 'about' => 'pages#about'
+  get 'about' => 'pages#about' #create about_path
+  get 'profile' => 'pages#profile' #create profile_path
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
