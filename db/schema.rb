f3c9f1de36b64d924791bf122526efaf9019cd23
Victor Vulovic <victor@humanify.co>
2015-04-10 11:07:08 -0700

    cleaned up listings scaffold. destroyed and reinstalled listings scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150406200822) do
+ActiveRecord::Schema.define(version: 20150410174532) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "description"
