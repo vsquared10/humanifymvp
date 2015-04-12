1d7d8e46130eb710c91b753828f3fda00ebff1e0
Victor Vulovic <victor@humanify.co>
2015-04-11 19:50:38 -0700

    Add names to users

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -4,4 +4,7 @@ class Listing < ActiveRecord::Base
 	has_attached_file :image, :styles => { :medium => "300x300>", :thumb => "100x100>" }, :default_url => "small_Humanify.png"
 	# Validate content type
   validates_attachment_content_type :image, :content_type => /\Aimage/
+
+  validates :description, presence: true
+  validates :image, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150410191114) do
+ActiveRecord::Schema.define(version: 20150412020610) do
 
   create_table "listings", force: :cascade do |t|
     t.string   "description"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 20150410191114) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
