0f913925efb7163745dc3671e14cf75fd066e694
Edward Reed <edwardareed@gmail.com>
2015-06-11 13:06:13 -0700

    Update Layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
   root 'listings#index'
 
-  resources :listings
   devise_for :users
+  resources :listings
 
   get 'about' => 'pages#about' #create about_path
   get 'profile' => 'pages#profile' #create profile_path
