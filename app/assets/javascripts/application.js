b4380a80e6b31c55f79e43f8ba2a58f6af470aa5
Edward Reed <edwardareed@gmail.com>
2015-07-27 16:55:42 -0700

    Add Backbone + React

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,8 +15,16 @@
 //= require bootstrap
 //= require jquery.turbolinks
 //= require turbolinks
+//= require react
+//= require react_ujs
+//= require components
 //= require masonry/jquery.masonry
 //= require menu-bar
 //= require slideout
 //= require slideout_trigger
 //= require webfonts
+//= require underscore
+//= require backbone
+//= require backbone_rails_sync
+//= require backbone_datalink
+//= require backbone/humanify
