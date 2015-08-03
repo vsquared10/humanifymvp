06bd24f1aa77ba7a2f2734289adfce32a0d514fe
Edward Reed <edwardareed@gmail.com>
2015-08-02 20:16:07 -0700

    Fix Merge

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -1,3 +1,5 @@
+// Read Sprockets README (https://github.com/sstephenson/sprockets#sprockets-directives) for details
+// about supported directives.
 //
 //= require jquery
 //= require jquery_ujs
@@ -10,40 +12,6 @@
 //= require backbone_datalink
 //= require react
 //= require react_ujs
-//= require ./lib/pusher
-//
-//
-//
-//= require ./ui/webfonts
-//= require bootstrap
-//= require ./lib/ratchet
-//= require ./lib/slideout
-//
-//= require ./ui/menu/inbox
-//= require ./ui/menu/slideout_trigger
-//
-//
-//
-//= require ./init/environment
-//= require ./init/pusher-init
-//
-//
-//= require ./lib/backbone-react-component
-//= require ./lib/cx
-//
-//= require ./front/backbone/humanify
-//= require ./front/backbone/models/notification
-//= require ./front/backbone/models/inbox
-//= require ./front/backbone/collections/notification
-//= require ./front/backbone/collections/inbox
-
-//
-//= require_tree ./front/components/inbox
-//= require_tree ./front/components/notifications
-//
-//= require ./front/events/notification
-//= require ./front/events/inbox
-=======
 //= require lib/pusher
 //
 //
@@ -76,4 +44,3 @@
 //= require front/events/notification
 //= require front/events/inbox
 //
->>>>>>> 877cffa29b277b8d3ffcdbfd6f7d7939f7178bc1
