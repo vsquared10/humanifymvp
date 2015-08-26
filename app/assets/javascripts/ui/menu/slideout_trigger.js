dc95935680485a3c7b39e87c78c1c1394f1bf3ad
Edward Reed <edwardareed@gmail.com>
2015-08-26 14:25:58 -0700

    Update Toggle SlideOut

--- app/assets/javascripts/ui/menu/slideout_trigger.js
+++ app/assets/javascripts/ui/menu/slideout_trigger.js
@@ -1,15 +1,17 @@
 $(document).ready(function(){
-  var slideout = new Slideout({
-    'panel': document.getElementById('panel'),
-    'menu': document.getElementById('mobile-menu'),
-    'padding': 256,
-    'side': 'right',
-    'tolerance': 70
-  });
+  if ($('.toggle-button')) {
+    var slideout = new Slideout({
+      'panel': document.getElementById('panel'),
+      'menu': document.getElementById('mobile-menu'),
+      'padding': 256,
+      'side': 'right',
+      'tolerance': 70
+    });
 
-  // Toggle button
-  document.querySelector('.toggle-button').addEventListener('click', function() {
-    slideout.toggle();
-    $('#mobile-menu').toggle();
-  });
+    // Toggle button
+    document.querySelector('.toggle-button').addEventListener('click', function() {
+      slideout.toggle();
+      $('#mobile-menu').toggle();
+    });
+  }
 });
