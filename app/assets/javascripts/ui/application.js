452254054507094dd07e5cd77d469dc90dc400c3
EdwardReed <edwardareed@gmail.com>
2016-01-31 22:22:08 -0800

    Add Notification Style
            * Add Notification Style
            * Add Notification Dismissal

--- app/assets/javascripts/ui/application.js
+++ app/assets/javascripts/ui/application.js
@@ -1,1 +1,9 @@
-$(document).ready(function(){});
+$(document).ready(function(){
+  var notification = $("#notification");
+  notification.after().click(function (e) {
+    console.log("click", e.offsetX )
+      if (e.offsetX >= ($("#notification").width() - 5)) {
+        notification.hide();
+      }
+  });
+});
